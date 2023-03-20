e7148a6c9d59493af571cc43555f784177cbf601
asd0905 <asd0905@naver.com>
2023-03-21 01:17:22 +0900

    webpack, package 옵션 수정

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,27 @@ module.exports = {
             tempalte: './public/index.html',
         }),
     ],
+    devtool: 'inline-source-map',
     mode: "development",
     optimization: { minimizer: [] },
+    target: 'web',
+    devServer: {
+        // dist 디렉토리를 웹 서버의 기본 호스트 위치로 설정
+        // contentBase: path.resolve(__dirname, './dist'),
+        // 인덱스 파일 설정
+        // index: 'index.html',
+        // 포트 번호 설정
+        port: 9000,
+        // 핫 모듈 교체(HMR) 활성화 설정
+        hot: true,
+        // gzip 압축 활성화
+        compress: true,
+        // dist 디렉토리에 실제 파일 생성
+        // writeToDisk: true,
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+    },
     module: {
         rules: [
             {
@@ -40,5 +59,9 @@ module.exports = {
                 ]
             }
         ]
-    }
+    },
+    performance: {
+        hints: process.env.NODE_ENV === 'production' ? "warning" : false
+    },
+
 }
